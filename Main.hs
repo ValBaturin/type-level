d60c900c8e75ee0f5791352a6f1587b4bd336ba6
Val <valeryxbaturin@gmail.com>
2020-01-22 23:17:02 +0300

    wow such a type-level function

--- Main.hs
+++ Main.hs
@@ -1,11 +1,21 @@
- {-# LANGUAGE StarIsType #-}
- {-# LANGUAGE DataKinds #-}
- {-# LANGUAGE TypeOperators #-}
- {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE StarIsType #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 import GHC.TypeLits
+import Data.Typeable
 
 type family Or (x :: Bool) (y :: Bool) :: Bool where
     Or 'True y = 'True
     Or 'False y = y
 
-main = putStrLn "type-level"
+typeName :: forall a. Typeable a => String
+typeName = show . typeRep $ Proxy @a
+
+data Typelevel
+main = putStrLn $ typeName @Typelevel
