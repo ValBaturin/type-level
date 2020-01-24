4cca8b18d16922d49f8dc5f98db10734390edf48
Val <valeryxbaturin@gmail.com>
2020-01-24 22:07:04 +0300

    heterogeneous lists

--- Main.hs
+++ Main.hs
@@ -8,9 +8,14 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
 {-# LANGUAGE GADTs #-}
+
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
  
 import GHC.TypeLits
 import Data.Typeable
+
+import Data.Kind (Constraint, Type)
 
 type family Or (x :: Bool) (y :: Bool) :: Bool where
     Or 'True y = 'True
@@ -36,4 +41,10 @@ data Expr a where
     Not :: Expr Bool -> Expr Bool
     If :: Expr Bool -> Expr a -> Expr a -> Expr a
 
+data HList (ts :: [Type]) where
+    HNil :: HList '[]
+    (:#) :: t -> HList ts -> HList (t ': ts)
+infixr 5 :#
 
+hHead :: HList (t ': ts) -> t
+hHead (t :# _) = t
