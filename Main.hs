e82a81b4d44befa3fda2d2409045aaf9cdba504d
Val <valeryxbaturin@gmail.com>
2020-01-24 21:54:38 +0300

    gadtsless

--- Main.hs
+++ Main.hs
@@ -8,7 +8,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
 {-# LANGUAGE GADTs #-}
-
+ 
 import GHC.TypeLits
 import Data.Typeable
 
@@ -22,9 +22,18 @@ typeName = show . typeRep $ Proxy @a
 data Typelevel
 main = putStrLn $ typeName @Typelevel
 
+data Expr_ a
+    = (a ~ Int) => LitInt_ Int
+    | (a ~ Bool) => LitBool_ Bool
+    | (a ~ Int) => Add_ (Expr_ Int) (Expr_ Int)
+    | (a ~ Bool) => Not_ (Expr_ Bool)
+    | If_ (Expr_ Bool) (Expr_ a) (Expr_ a)
+
 data Expr a where
     LitInt :: Int -> Expr Int
     LitBool :: Bool -> Expr Bool
     Add :: Expr Int -> Expr Int -> Expr Int
     Not :: Expr Bool -> Expr Bool
     If :: Expr Bool -> Expr a -> Expr a -> Expr a
+
+
